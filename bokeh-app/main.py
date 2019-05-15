01044057ff19de92ff35268d88909f83f91b1a03
Ishana Raina <ishana.raina@yai.org>
2019-05-15 12:26:19 -0400

    Created a Bokeh application folder with files correctly structured

---
+++ bokeh-app/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from os.path import dirname, join
+from bokeh.io import curdoc
+from bokeh.models.widgets import Tabs
+from bokeh.sampledata.us_states import data as states
+
+from scripts.histogram import histogram_tab
+#from scripts.draw_map import map_tab
+
+flights=pd.read_csv(join(dirname(__file__), 'data', 'flights.csv'), index_col=0).dropna()
+
+#map_data = pd.read_csv(join(dirname(__file__), 'data', 'flights_map.csv'), header=[0,1], index_col=0)
+
+tab1=histogram_tab(flights)
+#tab2=map_tab(map_data, states)
+
+tabs = Tabs(tabs=[tab1]) #add tab2
+curdoc().add_root(tabs)
