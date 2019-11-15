64ee52e9cd26055c6cc20bd3dd7bcc4d879c3424
Ishana Raina <49597585+IshanaRaina@users.noreply.github.com>
2019-11-14 18:55:14 -0800

    Update and rename mapIt.py to map_it.py

--- map_it.py
+++ map_it.py
@@ -1,5 +1,4 @@
 #! python3
-# mapIt.py - Launches a map in the browser using an address from the command line or clipboard.
 
 import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
@@ -9,8 +8,3 @@ else:
     address = pyperclip.paste()
 
 webbrowser.open('https://www.google.com/maps/place/' + address)
-
-
-
-
-
